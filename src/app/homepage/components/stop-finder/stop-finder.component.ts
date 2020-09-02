4197c8655dd06b4c7a9315b624c301db9b14fa43
Conor Haining <conor.haining@gmail.com>
2020-09-02 22:15:18 +0100

    feat: new stop finder component

---
+++ src/app/homepage/components/stop-finder/stop-finder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'stop-finder',
+  templateUrl: './stop-finder.component.html',
+  styleUrls: ['./stop-finder.component.scss']
+})
+export class StopFinderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
