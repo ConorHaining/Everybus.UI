9d69c2388d94f1d3a0043f1d8757c4be7b6a05e2
Conor Haining <conor.haining@gmail.com>
2020-09-02 22:41:01 +0100

    include stop finder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage/homepage.component';
+import { StopFinderComponent } from './homepage/components/stop-finder/stop-finder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    StopFinderComponent
   ],
   imports: [
     BrowserModule,
