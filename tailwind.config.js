8b8aeb942f17affe769640936f550597e7f377e3
Conor Haining <conor.haining@gmail.com>
2020-09-07 13:22:38 +0100

    fix: change green colour palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,27 @@ module.exports = {
   purge: [
     './src/**/*.html',
     './src/**/*.ts',
-    './src/styles/**',
-    './src/styles.scss',
   ],
   theme: {
     extend: {
       colors: {
         gray: {
           ...colors.gray,
-          'gray-body': '#1e3656',
-        }
+          'body': '#F4F1DE',
+        },
+
+        green: {
+          100: '#9AC1AE',
+          200: '#8DB9A4',
+          300: '#81B29A',
+          400: '#74AA90',
+          500: '#67A286',
+          600: '#5D987C',
+          700: '#558B72',
+          800: '#4E7E68',
+          900: '#46725E',
+        },
+
       }
     }
   },
