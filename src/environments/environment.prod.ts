4fe2b1a6e361a8a29683034a0cf79917c8a3317f
Conor Haining <conor.haining@gmail.com>
2020-09-08 11:17:21 +0100

    fix: use https tfe links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
 
   tfeOpenDataUrls: {
-    stops: 'http://tfe-opendata.com/api/v1/stops',
-    liveTimes: 'http://tfe-opendata.com/api/v1/live_bus_times/{stop_id}',
+    stops: 'https://tfe-opendata.com/api/v1/stops',
+    liveTimes: 'https://tfe-opendata.com/api/v1/live_bus_times/{stop_id}',
     route: 'https://tfeapp.com/api/website/route.php?service_name={route_name}'
   }
 };
