0ab2699dc2f4c9a3b1cdf4f0a0ba0ecc1333bc1a
Conor Haining <conor.haining@gmail.com>
2020-09-08 11:10:23 +0100

    fix: add prod urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+
+  tfeOpenDataUrls: {
+    stops: 'http://tfe-opendata.com/api/v1/stops',
+    liveTimes: 'http://tfe-opendata.com/api/v1/live_bus_times/{stop_id}',
+    route: 'https://tfeapp.com/api/website/route.php?service_name={route_name}'
+  }
 };
