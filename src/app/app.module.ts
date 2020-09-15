91c4a44441340b20c31c0de7fbc844f37f5a1df9
Conor Haining <conor.haining@gmail.com>
2020-09-15 19:56:53 +0100

    feat: merge stop selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StopFinderComponent } from './homepage/components/stop-finder/stop-finder.component';
-import { StopSelectorComponent } from './homepage/components/stop-selector/stop-selector.component';
 import { OverlayHostDirective } from './homepage/directives/overlay-host.directive';
 import { HomepageComponent } from './homepage/homepage/homepage.component';
+import { DestinationsOutputPipe } from './homepage/pipes/destinations-output.pipe';
 import { StopFilterPipe } from './homepage/pipes/stop-filter.pipe';
 import { MapPageComponent } from './map-page/map-page.component';
 import { DepartureDetailsComponent } from './stop-details-page/components/departure-details/departure-details.component';
-import { StopDetailsPageComponent } from './stop-details-page/stop-details-page.component';
-import { DestinationsOutputPipe } from './homepage/pipes/destinations-output.pipe';
 import { RelativeTimePipe } from './stop-details-page/pipes/relative-time.pipe';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { StopDetailsPageComponent } from './stop-details-page/stop-details-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     StopFinderComponent,
-    StopSelectorComponent,
     OverlayHostDirective,
     StopDetailsPageComponent,
     DepartureDetailsComponent,
