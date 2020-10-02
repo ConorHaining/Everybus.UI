22cd15a68cab3de9c986cdff78a40c96b5c72455
Conor Haining <conor.haining@gmail.com>
2020-10-02 19:46:07 +0100

    fix: unsubscribe from polling

--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { geoJSON, GeoJSON, latLng, LatLng, MapOptions, tileLayer } from 'leaflet';
+import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { VehicleTrackingService } from './../services/vehicle-tracking.service';
 
@@ -9,7 +10,7 @@ import { VehicleTrackingService } from './../services/vehicle-tracking.service';
   templateUrl: './map-page.component.html',
   styleUrls: ['./map-page.component.scss']
 })
-export class MapPageComponent implements OnInit {
+export class MapPageComponent implements OnInit, OnDestroy {
 
   options: MapOptions = {
     layers: [
@@ -26,6 +27,7 @@ export class MapPageComponent implements OnInit {
   vehicle: any;
   vehicleLayer: GeoJSON;
   vehicleId: string;
+  private vehicleUpdates: Subscription;
 
   constructor(
     private readonly vehicleTracking: VehicleTrackingService,
@@ -33,19 +35,21 @@ export class MapPageComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+
     this.route.paramMap.pipe(
       map(params => params.get('vehicleId'))
     ).subscribe(vehicleId => { this.vehicleId = vehicleId; });
 
-    const data = JSON.parse('{"features":[{"geometry":{"coordinates":[-3.234966,55.93347],"type":"Point"},"properties":{"heading":51,"colour":"#878787","text_colour":"#FFFFFF","name":"44","vehicleId":"875","last_update":"09/29/2020 22:36:12","destination":"Wallyford"},"type":"Feature"}, {"geometry":{"coordinates":[-3.203486,55.95071],"type":"Point"},"properties":{"heading":254,"colour":"#E8378C","text_colour":"#FFFFFF","name":"22","vehicleId":"368","last_update":"09/29/2020 22:55:12","destination":"Gyle Centre"},"type":"Feature"}],"type":"FeatureCollection"}');
-    this.vehicleLayer = geoJSON(data);
-
-    this.vehicleTracking.pollForAllVehicleLocations().subscribe(vehicleLocations => {
+    this.vehicleUpdates = this.vehicleTracking.pollForAllVehicleLocations().subscribe(vehicleLocations => {
       this.vehicle = Array.from(vehicleLocations.features).find((x: any) => x.properties.vehicleId === this.vehicleId);
       this.center = latLng(this.vehicle.geometry.coordinates[1], this.vehicle.geometry.coordinates[0]);
       const parsedGeoJson = geoJSON(this.vehicle);
       this.vehicleLayer = parsedGeoJson;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.vehicleUpdates.unsubscribe();
   }
 
 }
