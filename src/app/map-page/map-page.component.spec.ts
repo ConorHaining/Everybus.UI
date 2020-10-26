479da584507401244e3b97150511c158c7c91a03
Conor Haining <conor.haining@gmail.com>
2020-10-26 14:48:30 +0000

    test: map page details

--- src/app/map-page/map-page.component.spec.ts
+++ src/app/map-page/map-page.component.spec.ts
@@ -1,22 +1,70 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { of } from 'rxjs';
+import { VehicleLocations } from './../../test/data/vehicleLocations';
+import { VehicleTrackingService } from './../services/vehicle-tracking.service';
 import { MapPageComponent } from './map-page.component';
 
 describe('MapPageComponent', () => {
   let component: MapPageComponent;
   let fixture: ComponentFixture<MapPageComponent>;
+  let vehicleTrackingService: VehicleTrackingService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapPageComponent ]
+      imports: [LeafletModule],
+      declarations: [MapPageComponent],
+      providers: [
+        {
+          provide: VehicleTrackingService,
+          useValue: {
+            pollForAllVehicleLocations: jasmine.createSpy().and.returnValue(of(VehicleLocations))
+          }
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of(convertToParamMap({ vehicleId: '123' }))
+          }
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
+    vehicleTrackingService = TestBed.inject(VehicleTrackingService);
     fixture = TestBed.createComponent(MapPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  describe('ngOnInit', () => {
+
+    it('should get the vechicle id', () => {
+      expect(component.vehicleId).toBe('123');
+    });
+
+    it('should begin polling for vehicle updates', () => {
+      expect(vehicleTrackingService.pollForAllVehicleLocations).toHaveBeenCalled();
+    });
+
+    it('should find the vehicle', fakeAsync(() => {
+      tick();
+      expect(component.vehicle).toBeDefined();
+    }));
+
+    it('should center the map onto the vehicle', fakeAsync(() => {
+      tick();
+      expect(component.center).toBeDefined();
+    }));
+
+    it('should have a vehicle layer to display', fakeAsync(() => {
+      tick();
+      expect(component.vehicleLayer).toBeDefined();
+    }));
+
   });
 
   it('should create', () => {
---
+++ src/test/data/vehicleLocations.ts
@@ -0,0 +1,23 @@
+export const VehicleLocations = {
+    features: [
+      {
+        geometry: {
+          coordinates: [
+            -3.047204,
+            55.94069
+          ],
+          type: 'Point'
+        },
+        properties: {
+          heading: 172,
+          colour: '#CA7416',
+          text_colour: '#FFFFFF',
+          name: '30',
+          vehicleId: '123',
+          last_update: '10/26/2020 14:10:35',
+          destination: 'Musselburgh'
+        },
+        type: 'Feature'
+      }
+    ]
+  };
