e8ca1ec91634442f8d787a5fb0c986002ca644b5
Conor Haining <conor.haining@gmail.com>
2020-10-26 15:24:15 +0000

    test: relative pipe

--- src/app/pipes/relative-time.pipe.spec.ts
+++ src/app/pipes/relative-time.pipe.spec.ts
@@ -12,22 +12,39 @@ describe('RelativeTimePipe', () => {
     expect(pipe).toBeTruthy();
   });
 
+  it('it should return when null is supplied', () => {
+    const pipe = new RelativeTimePipe();
+
+    const result = pipe.transform(null);
+
+    expect(result).toBeUndefined();
+  });
+
   it('it should return the relative time from now when over a minute', () => {
     const utcTimestamp = '2020-09-08T13:05:00+00:00';
     const pipe = new RelativeTimePipe();
 
     const result = pipe.transform(utcTimestamp);
 
-    expect(result).toEqual('in 5 mins');
+    expect(result).toEqual('in 5 minutes');
   });
 
-  it('it should return \'NOW\' when under a minute', () => {
+  it('it should return \'Very soon\' when under two minutes', () => {
     const utcTimestamp = '2020-09-08T13:01:00+00:00';
     const pipe = new RelativeTimePipe();
 
     const result = pipe.transform(utcTimestamp);
 
-    expect(result).toEqual('Now');
+    expect(result).toEqual('Very soon');
+  });
+
+  it('it should return \'Departed\' when it after the current time', () => {
+    const utcTimestamp = '2020-09-08T12:55:00+00:00';
+    const pipe = new RelativeTimePipe();
+
+    const result = pipe.transform(utcTimestamp);
+
+    expect(result).toEqual('Departed');
   });
 
   it('it should return the absolute time from now when in 45 minutes or more', () => {
