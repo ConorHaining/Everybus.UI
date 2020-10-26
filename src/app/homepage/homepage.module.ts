4e27b353b4667f8542cd8e313946a434c0abdf13
Conor Haining <conor.haining@gmail.com>
2020-10-26 13:20:29 +0000

    refactor: remove unused directive

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,9 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PipesModule } from '../pipes/pipes.module';
-import { ServicesModule } from './../services/services.module';
 import { StopFinderComponent } from './components/stop-finder/stop-finder.component';
-import { OverlayHostDirective } from './directives/overlay-host.directive';
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
 
@@ -13,7 +11,6 @@ import { HomepageComponent } from './homepage/homepage.component';
 @NgModule({
   declarations: [
     HomepageComponent,
-    OverlayHostDirective,
     StopFinderComponent,
   ],
   imports: [
