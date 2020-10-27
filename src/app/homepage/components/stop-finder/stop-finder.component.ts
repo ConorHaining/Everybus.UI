fb777b40bcb10e7d98f3700e3ff5a7a01ad00066
Conor Haining <conor.haining@gmail.com>
2020-10-27 15:18:29 +0000

    resolve build problems

--- src/app/homepage/components/stop-finder/stop-finder.component.ts
+++ src/app/homepage/components/stop-finder/stop-finder.component.ts
@@ -1,6 +1,5 @@
 import { ActiveDescendantKeyManager } from '@angular/cdk/a11y';
-import { Component, OnInit, ViewChildren, QueryList, HostListener, AfterContentInit, AfterViewInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { AfterViewInit, Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { Stop } from 'src/app/models/Stop';
 import { StopsService } from 'src/app/services/stops.service';
 import { StopOptionComponent } from '../stop-option/stop-option.component';
@@ -43,19 +42,8 @@ export class StopFinderComponent implements OnInit, AfterViewInit {
     this.options.changes.subscribe(x => this.activeDescendant = null);
   }
 
-  pickRandom(): void {
-    const index = Math.floor(Math.random() * this.stops.length);
-    const stop = this.stops[index];
-
-    this.router.navigate(['stop', stop.atco_code]);
-  }
-
   togglePicker(): void {
     this.showPicker = !this.showPicker;
-  }
-
-  goToDepartures(atcoCode: string): void {
-    this.router.navigate(['stop', atcoCode]);
   }
 
   activeStopChange(atcoCode: string): void {
--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,11 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PipesModule } from '../pipes/pipes.module';
-import { StopFinderComponent } from './components/stop-finder/stop-finder.component';
 import { AutocompleteModule } from './../common/autocomplete/autocomplete.module';
 import { StopFinderComponent } from './components/stop-finder/stop-finder.component';
 import { StopOptionComponent } from './components/stop-option/stop-option.component';
-import { OverlayHostDirective } from './directives/overlay-host.directive';
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
 
